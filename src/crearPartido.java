4cc2968bef83a30c159fd488dce1c50ceafb2418
dgonrom2803 <dgonrom2803@g.educaand.es>
2022-12-21 11:55:48 +0100

    Revisión 4. INICIALES - Javadoc CrearPartido.java

--- src/crearPartido.java
+++ src/crearPartido.java
@@ -1,21 +1,28 @@
 
 import java.awt.event.KeyEvent;
 
-
-
+/**
+ *
+ * @author dgonrom2803
+ */
 public class crearPartido extends javax.swing.JFrame
 {
    
     
     private Apuesta v;
     
-   
+    /**
+     *
+     */
     public crearPartido()
     {
         initComponents();
     }
 
-   
+    /**
+     *
+     * @param ventana
+     */
     public crearPartido(Apuesta ventana)
     {
         initComponents();
@@ -70,6 +77,10 @@ public class crearPartido extends javax.swing.JFrame
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    /**
+     *
+     * @param p
+     */
     public void añadePartido(String p)
     {
         v.partidoNuevo(p);
@@ -88,7 +99,10 @@ public class crearPartido extends javax.swing.JFrame
         }
     }//GEN-LAST:event_tPartidoKeyPressed
 
-    
+    /**
+     *
+     * @param args
+     */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
